4b96f137533f3edc14236dbaf6cb12024b6151a0
Sushmitha Baliga B <masrbaliga@gmail.com>
2025-08-18 11:49:11 +0530

    updated SafeSecure.css to responsiveness

--- src/components/Mainsection/SafeSecure.jsx
+++ src/components/Mainsection/SafeSecure.jsx
@@ -3,26 +3,20 @@ import "./SafeSecure.css";
 
 const SafeSecure = () => {
   return (
-      <div id="securitycard" data-theme = 'light'className="safe-secure-container" style={{
-  width: 1200,
-  height: 450,
-  angle: "0deg",
-  opacity: 1,
-  
-  borderRadius: 37.03
-}}>
+    <div id="securitycard" data-theme="light" className="safe-secure-container">
       <div className="safe-secure-content">
         {/* Left Text Section */}
         <div className="safe-secure-text">
           <h2 className="safe-secure-title">Safe & Secure</h2>
           <p className="safe-secure-description">
-            At BAFT, we know trust isn't built in a day. That's why every payment,
-            every detail, and every account is protected with care. No hidden
-            risks. Just the security you deserve while managing your money.
+            At BAFT, we know trust isn't built in a day. That's why every
+            payment, every detail, and every account is protected with care. No
+            hidden risks. Just the security you deserve while managing your
+            money.
           </p>
         </div>
 
-        {/* Right Logo Section (SVG only) */}
+        {/* Right Logo Section */}
         <div className="safe-secure-logo">
           <div className="security-logo-wrapper">
             <img
