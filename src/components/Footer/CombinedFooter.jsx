ade65a77c4de83cbed69620361236f866cdd8aa3
Your Name <you@example.com>
2025-09-08 15:15:32 +0530

    foot

--- src/components/Footer/CombinedFooter.jsx
+++ src/components/Footer/CombinedFooter.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+2import React, { useState, useEffect, useRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faInstagram,
